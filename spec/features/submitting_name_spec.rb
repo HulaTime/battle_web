fdee2f3d44f0039316610c0936626ac819239c46
Max Provin <maxprovin@outlook.com>
2016-04-26 16:09:13 +0100

    Step 21 - test helpers

--- spec/features/submitting_name_spec.rb
+++ spec/features/submitting_name_spec.rb
@@ -1,15 +1,11 @@
 feature "Player registration", :type => :feature do
   scenario "User enters name" do
-    visit "/"
-    fill_in "Player 1 name:", :with => "Mr.Jones"
-    fill_in "Player 2 name:", :with => "Mrs.Jones"
-    click_button "Submit"
-
+    sign_in_and_play
     expect(page).to have_text("Player 1: Mr.Jones vs Player 2: Mrs.Jones FIGHT!")
   end
 
   scenario "Player 1 has hit points" do
-  	visit "/play"
-  	expect(page).to have_text("HP : 100")
+    sign_in_and_play
+  	expect(page).to have_text("Mr.Jones HP : 100")
   end
 end
