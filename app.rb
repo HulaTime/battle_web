155d1e0e915b11164fe5ffae604c9114fd69f755
Max Provin <maxprovin@outlook.com>
2016-04-26 14:07:24 +0100

    Step 18 - entering players

--- app.rb
+++ app.rb
@@ -1,7 +1,16 @@
 require 'sinatra/base'
 
-class GigaWars < Sinatra::Application
-	get '/' do
-		'Hello world!'
-	end
+class Battle < Sinatra::Base
+  get '/' do
+    erb(:index)
+  end
+
+  post '/names' do
+    @Player1 = params[:Player1name]
+    @Player2 = params[:Player2name]
+    erb(:play)
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
---
+++ spec/features/submitting_name_spec.rb
@@ -0,0 +1,10 @@
+feature "Player registration", :type => :feature do
+  scenario "User enters name" do
+    visit "/"
+    fill_in "Player 1 name:", :with => "Mr.Jones"
+    fill_in "Player 2 name:", :with => "Mrs.Jones"
+    click_button "Submit"
+
+    expect(page).to have_text("Player 1: Mr.Jones vs Player 2: Mrs.Jones FIGHT!")
+  end
+end
